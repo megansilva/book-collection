10313afb9f994712153c5634bd2e7951832b1726
Megan Silva <megansilva@Megans-MacBook-Pro.local>
2022-08-06 11:53:19 -0400

    trying again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/collection', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:3001/book-collection', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
